e18ceaa4cc8516ba62ba6a0c96f5e4adfedca7fb
MGSousa <31368750+MGSousa@users.noreply.github.com>
2023-09-14 22:44:23 +0100

    Minor improvements on qtile install script

--- .config/qtile/install.sh
+++ .config/qtile/install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "Installing first xcffib then cairocffi and finally qtile"
+echo "Installing first xcffib, cairocffi then qtile"
 pip install 'xcffib >= 0.10.1' && \
 pip install --no-cache-dir 'cairocffi >= 0.9.0' && \
-pip install qtile psutil
+pip install qtile psutil dbus-next
+
+# FIX qtile lib on new versions of python
+pip install --no-build-isolation git+https://github.com/qtile/qtile
